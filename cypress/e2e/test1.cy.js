359a80727d2e28dfbd2ed2ea5322efbcc079684c
yogi mustofa <yogimustofa6@gmail.com>
2024-04-29 16:37:43 +0700

    simplidots

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -9,5 +9,13 @@ describe('login', () => {
         cy.get('#login_button').click()//click button login
         cy.location("pathname").should("equal", "/u/admintester")
         
+        cy.get('.logo > img').click()//back to main menu or home
+            //cy.get('#trending_scroller > .column_content > :nth-child(1) > div.image > .options > .no_click > .glyphicons_v2').click()
+       
+        //#mark vaforite  1 movie"#//    
+        cy.get(':nth-child(1) > div.image > .wrapper > .image > .poster').click()
+        cy.get('#favourite').click()
+        cy.location("pathname").should("equal", "/tv/106379-fallout")
+ 
     })
 })
